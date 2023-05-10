acdd360c670091c4f09ce4e592ad1c7147e83cdc
Mohamed <mohamed.elfirdoussi@um5r.ac.ma>
2023-05-10 11:17:03 +0100

    allowed hosts

--- project/settings.py
+++ project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-b=he9c_j_um+4_pk-t-k-(9$fri!$c6^mcg(rz9d&4vvo!=md7"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
